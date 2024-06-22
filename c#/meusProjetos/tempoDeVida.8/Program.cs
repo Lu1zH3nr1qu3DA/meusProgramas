7c2b11aa6d808a54b8dfd86c4fe1ecb9ed8d1997
Luiz Henrique Dainez Alecrim <luiz.hdalecrim@outlook.com>
2024-06-22 17:10:52 -0300

    dotnet8

---
+++ c#/meusProjetos/tempoDeVida.8/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace tempoDeVida._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime nascimento;
+            int tempo;
+
+            Console.Write("Insira sua data de nascimento: ");
+            nascimento = Convert.ToDateTime(Console.ReadLine());
+
+            tempo = Convert.ToInt16(DateTime.Now.Subtract(nascimento).Days);
+
+            Console.WriteLine($"\nVocê viveu, aproximadamente, {tempo} dias.");
+
+            Console.Write("\n\nPressione qualquer tecla para fechar. . . ");
+            Console.ReadKey();
+        }
+    }
+}
