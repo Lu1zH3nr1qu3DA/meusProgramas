755b0bfe5e15c828937108d3d6febb7a5ce6a34a
Luiz Henrique Dainez Alecrim <luiz.hdalecrim@outlook.com>
2024-06-27 01:18:21 -0300

    Projeto "Folha de Pagamento" v1 concluída

--- c#/senac/POO/POOFolhaPagamento/Funcionarios.cs
+++ c#/senac/POO/POOFolhaPagamento/Funcionarios.cs
@@ -12,31 +12,31 @@ namespace POOFolhaPagamento
         public double Salario { get; set; }
         public double ValInss { get; set; }
         public double ValIR { get; set; }
-        public double ValorLiquido { get; set; }
+        public double ValLiquido { get; set; }
 
         public void Inss() 
         {
             if (this.Salario <= 1412)
             {
-                ValInss = Salario * (7.5 / 100);
+                this.ValInss = Salario * (7.5 / 100);
             }
             else
             {
-                if (this.Salario <= 2666.68)
+                if (1412 < this.Salario && this.Salario <= 2666.68)
                 {
-                    ValInss = Salario * (9 / 100);
+                    this.ValInss = Salario * (9 / 100);
                 }
                 else
                 {
-                    if (this.Salario <= 4000.03)
+                    if (2666.68 < this.Salario && this.Salario <= 4000.03)
                     {
-                        ValInss = Salario * (12 / 100);
+                        this.ValInss = Salario * (12 / 100);
                     }
                     else
                     {
-                        if (this.Salario <= 7786.02)
+                        if (4000.03 < this.Salario && this.Salario <= 7786.02)
                         {
-                            ValInss = Salario * (14 / 100);
+                            this.ValInss = Salario * (14 / 100);
                         }
                     }
                 }
@@ -47,29 +47,34 @@ namespace POOFolhaPagamento
         {
             if (this.Salario <= 1412)
             {
-                ValIR = Salario * (7.5 / 100);
+                this.ValIR = Salario * (7.5 / 100);
             }
             else
             {
                 if (this.Salario <= 2666.68)
                 {
-                    ValIR = Salario * (15 / 100);
+                    this.ValIR = Salario * (15 / 100);
                 }
                 else
                 {
                     if (this.Salario <= 4000.03)
                     {
-                        ValIR = Salario * (22.5 / 100);
+                        this.ValIR = Salario * (22.5 / 100);
                     }
                     else
                     {
                         if (this.Salario <= 7786.02)
                         {
-                            ValIR = Salario * (27.5 / 100);
+                            this.ValIR = Salario * (27.5 / 100);
                         }
                     }
                 }
             }
+        }
+
+        public void VLiquido()
+        {
+            this.ValLiquido = this.Salario - this.ValInss - this.ValIR;
         }
     }
 }
--- c#/senac/POO/POOFolhaPagamento/Program.cs
+++ c#/senac/POO/POOFolhaPagamento/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +12,99 @@ namespace POOFolhaPagamento
     {
         static void Main(string[] args)
         {
-            List<Funcionarios> funcionarios = new List<Funcionarios>();
+            List<Funcionarios> listFuncionarios = new List<Funcionarios>();
 
             string resposta = "";
 
-            while (resposta != "4")
+            while (resposta != "5")
             {
+                Console.Clear();
+                Console.WriteLine(".......Folha de Pagamento (CRUD).......");
+                Console.WriteLine("............Menu de Opções.............\n");
+                Console.WriteLine("Digite uma opção.");
+                Console.WriteLine("(1) - Cadastrar um Funcionário");
+                Console.WriteLine("(2) - Listar Funcionários");
+                Console.WriteLine("(3) - Alterar um Funcionário");
+                Console.WriteLine("(4) - Excluir um Funcionário");
+                Console.WriteLine("(5) - Sair\n");
 
+                resposta = Console.ReadLine();
+
+                switch (resposta)
+                {
+                    case "1":
+                        Funcionarios objFuncionarios = new Funcionarios();
+                        Console.Write("Insira o Nome do funcionário: ");
+                        objFuncionarios.Nome = Console.ReadLine();
+                        Console.Write($"Insira o valor do Salário de {objFuncionarios.Nome}: R$");
+                        objFuncionarios.Salario = Convert.ToDouble(Console.ReadLine());
+
+                        objFuncionarios.Inss();
+                        objFuncionarios.IR();
+                        objFuncionarios.VLiquido();
+
+                        listFuncionarios.Add(objFuncionarios);
+
+                        break;
+                    case "2":
+                        int tableWidth = 73;
+
+                        Console.Clear();
+                        PrintLine();
+                        PrintRow("Nome", "Salário", "INSS", "IR", "Valor Líquido");
+                        PrintLine();
+                        foreach (Funcionarios obj in listFuncionarios)
+                        {
+                            PrintRow($"{obj.Nome}", $"R${Math.Round(obj.Salario, 2)}", $"R${Math.Round(obj.ValInss, 2)}", $"R${Math.Round(obj.ValIR, 2)}", $"R${Math.Round(obj.ValLiquido, 2)}");
+                        }
+                        PrintLine();                        
+
+                        void PrintLine()
+                        {
+                            Console.WriteLine(new string('-', tableWidth));
+                        }
+
+                        void PrintRow(params string[] columns)
+                        {
+                            int width = (tableWidth - columns.Length) / columns.Length;
+                            string row = "|";
+
+                            foreach (string column in columns)
+                            {
+                                row += AlignCentre(column, width) + "|";
+                            }
+
+                            Console.WriteLine(row);
+                        }
+
+                        string AlignCentre(string text, int width)
+                        {
+                            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+                            if (string.IsNullOrEmpty(text))
+                            {
+                                return new string(' ', width);
+                            }
+                            else
+                            {
+                                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+                            }
+                        }
+                        break;
+                    case "3":
+                    
+                        break;
+                    case "4":
+                    
+                        break;
+                    case "5":
+                        break;
+                    default:
+                        Console.WriteLine("\nErro: A opção inserida não existe.");
+                        break;
+                }
+                Console.Write("\n\n\nPressione qualquer tecla para continuar. . . ");
+                Console.ReadKey();
             }
         }
     }
